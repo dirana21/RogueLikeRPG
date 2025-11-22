a2df089e3c26e3d0777e4d641a334a352aca941a
Dirana <levchenko.98989878@gmail.com>
2025-11-22 16:53:42 +0200

    Add a talents and group them by branches

---
+++ Assets/Scripts/Talents/UI/TalentTooltipTrigger.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TalentTooltipTrigger : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private TalentTooltipUI tooltipUI;
+
+    private TalentNodeView _view;
+    private bool shown;
+
+    void Awake()
+    {
+        _view = GetComponent<TalentNodeView>();
+    }
+
+    public void SetTooltip(TalentTooltipUI ui) => tooltipUI = ui;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!_view || _view.Data == null || !tooltipUI) return;
+
+        if (eventData.button != PointerEventData.InputButton.Right)
+            return;
+
+        if (shown)
+        {
+            tooltipUI.Hide();
+            shown = false;
+        }
+        else
+        {
+            tooltipUI.Show(_view.Data, Input.mousePosition);
+            shown = true;
+        }
+    }
+}
